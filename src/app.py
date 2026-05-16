ad12dab2ba93863838812d38a1b5cbfcf8751592
Soso <slawson2006.2@gmail.com>
2026-05-16 13:02:27 +0100

    just checking

--- src/app.py
+++ src/app.py
@@ -3,10 +3,12 @@ from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'x' #change
+app.config["SECRET_KEY"] = 'x' # dont change
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
 
 @app.route("/")
 def home():
